57bc1b40157a030fdbdff92e0b7a32de083fc22c
ana0 <sarah.anne.friend@gmail.com>
2022-02-18 11:47:01 +0100

    New ipfs uploads and close ports

--- scripts/ipfs-upload.sh
+++ scripts/ipfs-upload.sh
@@ -10,8 +10,20 @@ docker exec ipfs ipfs add -w /home/misc/off.png
 
 docker exec ipfs ipfs add -w /home/misc/lifeforms-contract.json
 
+docker exec ipfs ipfs add -w /home/misc/glitch.mp4
+
+docker exec ipfs ipfs add -w /home/misc/glitch-cover.gif
+
+docker exec ipfs ipfs add -w /home/misc/glitch-metadata.json
+
+docker exec ipfs ipfs add -w /home/misc/glitch-contract.json
+
+docker exec ipfs ipfs add -w /home/misc/coinbg.jpg
+
+docker exec ipfs ipfs add -w /home/misc/clickmine-contract.json
+
 FILES=./clickmine/*
 for f in $FILES
 do
-  docker exec ipfs ipfs add /home/"$(printf %q "$f")"
+  docker exec ipfs ipfs add -w /home/"$(printf %q "$f")"
 done
