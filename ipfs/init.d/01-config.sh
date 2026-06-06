632366b632a1850a2f5a3b3de6c6719f44bac5b1
ana0 <sarah.anne.friend@gmail.com>
2026-06-06 17:16:01 +0200

    Fix ipfs init script

--- ipfs/init.d/01-config.sh
+++ ipfs/init.d/01-config.sh
@@ -1,11 +1,8 @@
-  #!/bin/sh
-  # Runs on every container start, BEFORE the ipfs daemon launches.
-  # `ipfs config` operates directly on the repo file, so it doesn't need the daemon running.
-  # These commands are idempotent.
+ #!/bin/sh
   set -e
 
   ipfs config Datastore.StorageMax "20GB"
-  ipfs config Datastore.StorageGCWatermark 90
+  ipfs config --json Datastore.StorageGCWatermark 90
   ipfs config --json Datastore.GCPeriod '"1h"'
 
   echo "ipfs config applied: StorageMax=20GB, GCWatermark=90, GCPeriod=1h"
